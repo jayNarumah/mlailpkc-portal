bc4adb283c7ae697b4c8ea352096e69549ee2d2f
jayNarumah <jawadrumah@gmail.com>
2024-05-30 15:33:47 +0100

    notifications introduced

--- src/app/layout/app.layout.component.ts
+++ src/app/layout/app.layout.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2, ViewChild, computed, signal } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
 import { LayoutService } from "./service/app.layout.service";
 import { AppSidebarComponent } from "./components/app.sidebar.component";
 import { AppTopBarComponent } from './components/app.topbar.component';
+import { Message } from 'primeng/api';
+import { NotificationEndpoint } from 'src/api/endpoints/notification.endpoint';
+import { NoticationResource } from 'src/api/models/notication.model';
 
 @Component({
     selector: 'app-layout',
     templateUrl: './app.layout.component.html'
 })
-export class AppLayoutComponent implements OnDestroy {
-
+export class AppLayoutComponent implements OnInit, OnDestroy {
+    notfications = signal<NoticationResource[]>([]);
+    messages = computed(() => this.notfications().map(item => item.title));
     overlayMenuOpenSubscription: Subscription;
 
     menuOutsideClickListener: any;
@@ -21,7 +25,11 @@ export class AppLayoutComponent implements OnDestroy {
 
     @ViewChild(AppTopBarComponent) appTopbar!: AppTopBarComponent;
 
-    constructor(public layoutService: LayoutService, public renderer: Renderer2, public router: Router) {
+    constructor(
+        private readonly notificationEndpoint: NotificationEndpoint,
+        public layoutService: LayoutService,
+        public renderer: Renderer2,
+        public router: Router) {
         this.overlayMenuOpenSubscription = this.layoutService.overlayOpen$.subscribe(() => {
             if (!this.menuOutsideClickListener) {
                 this.menuOutsideClickListener = this.renderer.listen('document', 'click', event => {
@@ -55,6 +63,16 @@ export class AppLayoutComponent implements OnDestroy {
                 this.hideMenu();
                 this.hideProfileMenu();
             });
+    }
+
+    ngOnInit(): void {
+        this.notificationEndpoint.list().subscribe({
+            next: (response) => {
+                this.notfications.set(response.data);
+            },
+            error: (err) => console.log(err)
+
+        });
     }
 
     hideMenu() {
--- src/app/pages/landing/header-page/header-page.component.ts
+++ src/app/pages/landing/header-page/header-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, computed, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AuthLoggedInUserDto } from 'src/api/models/auth.request';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { UiModule } from 'src/app/ui/ui.module';
 
@@ -11,6 +13,21 @@ import { UiModule } from 'src/app/ui/ui.module';
     styleUrl: './header-page.component.scss'
 })
 export class HeaderPageComponent {
-    constructor(public router: Router, public readonly layoutService: LayoutService) { }
 
+    @ViewChild('menubutton') menuButton!: ElementRef;
+
+    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+
+    @ViewChild('topbarmenu') menu!: ElementRef;
+
+    constructor(
+        public layoutService: LayoutService,
+        private readonly router: Router,
+    ) {
+    }
+
+    ngOnInit() {
+
+    }
 }
+
